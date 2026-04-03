c930104a7e39a2d513221b1cf1228eb1ceca659e
sb11-1 <sbajwa2@csus.edu>
2026-04-02 21:50:43 -0700

    This commit is for adding a header with the company logo in the customer portal with a link that redirects to the landing page.

--- src/pages/CustomerPortal.jsx
+++ src/pages/CustomerPortal.jsx
@@ -1,2 +1,21 @@
 //Test file for portal
-// test 2
+
+import { Link } from 'react-router-dom'
+
+export default function CustomerPortal() {
+    
+    return(
+        //scrum 39 to navigate to landing page from logo
+        //create the header for the page
+        //the header contains the company logo and when clicked, will redirect to Home.jsx
+        //here the logo png is added and the to Home.jsx link is in App.jsx
+        //the logo png is in assests and is named logo.png
+        <header className="header">
+            <div className="container header-inner">
+                 <Link to="/" className="logo" aria-label="Prasad's Cleaning Services LLC">
+                 <img className="logo-img" src="/assets/logo.png" alt="Prasad's Cleaning Services LLC" />
+                </Link>
+            </div>
+        </header>
+    )
+}
